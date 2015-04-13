8cd314159836fb9241f9a030aec92492727942bb
Ali Hammad <syedalihammadshah@googlemail.com>
2015-04-13 18:56:11 +0500

    building transmitters for Suites

--- main.go
+++ main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
-	"log"
-	"os"
-	"os/exec"
+	"errors"
+
+	"github.com/alihammad-gist/sniffy"
+)
+
+var (
+	ErrNoDirProvided = errors.New("Suite don't have any directory")
 )
 
 type (
 	Suite struct {
-		Dirs []string `json:"dirs"`
-		Exts []string `json:"exts"`
-		Cmds []string `json:"cmds"`
-		Src  string   `json:"src"`
-		Dest string   `json:"dest"`
+		Dirs  []string `json:"dirs"`
+		Exts  []string `json:"exts"`
+		Root  string   `json:"root"`
+		Cmds  []string `json:"cmds"`
+		Src   string   `json:"src"`
+		Dest  string   `json:"dest"`
+		Label string   `json:"label"`
 	}
 )
 
@@ -36,19 +42,9 @@ type (
 */
 
 func main() {
-	f, err := os.Open("file.txt")
-	defer f.Close()
 
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = Pipe(
-		f,
-		os.Stdout,
-		exec.Command("grep", "asd"),
-		exec.Command("grep", ";"),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
+}
+
+func getTransmitter(s *Suite) (*sniffy.EventTransmitter, error) {
+
 }
